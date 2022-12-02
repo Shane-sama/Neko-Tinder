c3ed8e5acde843fc1c598e8b9bee642fae6dc856
Shane-sama <shane.roellin@icloud.com>
2022-12-02 08:36:06 +0100

    Fixed Image
    Image wasn't showing, now it does!

--- Neko-Tinder/app.js
+++ Neko-Tinder/app.js
@@ -2,5 +2,6 @@ fetch('https://nekos.best/api/v2/neko')
   .then(response => response.json())
   .then(json => {
     console.log(json.results[0].url)
-    document.getElementById("neko").src = json
-    })
+    img_url = json.results[0].url
+    document.getElementById("neko").src = img_url
+  })
